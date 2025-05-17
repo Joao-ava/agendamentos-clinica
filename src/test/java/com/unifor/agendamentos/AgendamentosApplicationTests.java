4588901237ffe54ca45ddc5919582b803fc23abc
Joao-ava <ajoao781238@gmail.com>
2025-05-17 16:35:56 -0300

    add: base project

---
+++ src/test/java/com/unifor/agendamentos/AgendamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifor.agendamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
