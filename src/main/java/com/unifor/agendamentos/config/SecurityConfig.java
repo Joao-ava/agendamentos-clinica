3a7d78fa6033bf5d5a3a52b505d68a23814c2836
Joao-ava <ajoao781238@gmail.com>
2025-06-08 20:43:33 -0300

    add: logout option in navbar

--- src/main/java/com/unifor/agendamentos/config/SecurityConfig.java
+++ src/main/java/com/unifor/agendamentos/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.unifor.agendamentos.config;
 import com.unifor.agendamentos.enums.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +25,11 @@ public class SecurityConfig {
                 .formLogin(login -> login
                         .usernameParameter("email") // Define que o login será feito pelo email
                         .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
                 );
 
         return http.build();
@@ -31,6 +38,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
 
